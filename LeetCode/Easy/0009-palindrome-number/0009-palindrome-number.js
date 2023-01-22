0e4b9b419d6a103ca44ceee232232066616486c2
Jay <chlwodnjs34@gmail.com>
2023-01-22 14:46:19 +0900

    Time: 148 ms (98.85%), Space: 50 MB (95.09%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.js
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.js
@@ -3,9 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(x<0) {
-        return false
+    let y = 0
+    let newX = x;
+    while(newX > 0) {
+        y = y*10 + newX%10;
+        newX = Math.floor(newX/10)
+
     }
-    
-    return Number(x.toString().split("").reverse().join("")) === x ? true : false
+
+    return y === x ? true : false;
 };
