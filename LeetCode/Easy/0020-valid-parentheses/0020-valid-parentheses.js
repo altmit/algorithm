1320785fec7ac67db27e703573055077ec6b9672
Jay <chlwodnjs34@gmail.com>
2023-01-28 16:00:29 +0900

    Time: 94 ms (34.65%), Space: 42.4 MB (58.44%) - LeetHub

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const obj = {
+        "[" : "]",
+        "(" : ")",
+        "{" : "}"
+    }
+    let stack = [];
+    s= s.split("");
+    while(s.length > 0) {
+        let bracket = s.shift();
+        if(stack.length === 0) {
+            stack.push(bracket);
+            continue;
+        }
+        if(bracket in obj) {
+            stack.push(bracket);
+            continue ;
+        }
+        if(obj[stack[stack.length-1]] === bracket) {
+            stack.pop();
+            continue ;
+        }
+        
+        return false;
+    }
+    return stack.length === 0 ? true : false
+};
