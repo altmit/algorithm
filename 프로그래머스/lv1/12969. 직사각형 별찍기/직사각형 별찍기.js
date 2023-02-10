197591ca35cd31eb4235d0ceb24d0670ea19fb44
Jay <chlwodnjs34@gmail.com>
2023-02-10 11:50:32 +0900

    [level 1] Title: 직사각형 별찍기, Time: 43.11 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,7 +1,6 @@
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
-    const n = data.split(" ");
-    const a = Number(n[0]), b = Number(n[1]);
+    const [a,b] = data.split(" ");
     var star = "*".repeat(a)+"\n";
     console.log(star.repeat(b));
 });
