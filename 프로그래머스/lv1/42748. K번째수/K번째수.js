379eb3db1eba1a80b2f446f30608eae6ac68c1a9
Jay <chlwodnjs34@gmail.com>
2023-02-20 22:51:33 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,6 +1,6 @@
 function solution(array, commands) {
     var answer = []
-    commands.map((x) => {
+    commands.forEach((x) => {
          answer.push(array.slice(x[0]-1,x[1]).sort((a,b) => a-b)[x[2]-1] )
     })
     return answer;
