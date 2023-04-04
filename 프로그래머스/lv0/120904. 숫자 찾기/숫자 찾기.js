104628c3575bcaf8cf2b8675a24dd5a278d8c429
Jay <chlwodnjs34@gmail.com>
2023-04-05 00:35:52 +0900

    [level 0] Title: 숫자 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,5 +1,3 @@
 function solution(num, k) {
-    const inedx = String(num).split("").indexOf(String(k));
-    
-    return inedx !== -1 ? inedx+1 : inedx
+    return ("."+num).split("").indexOf(""+k)
 }
