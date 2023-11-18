0d104f1da271548424fb0d371ad144377c4f8f2e
Jay <chlwodnjs34@gmail.com>
2023-11-18 17:33:29 +0900

    [Gold IV] Title: 공유기 설치, Time: 332 ms, Memory: 51112 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+
+const [N, C, ...arr] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(/\s/)
+  .map(Number);
+arr.sort((a, b) => a - b);
+
+function solve(arr, C) {
+  let start = 1;
+  let end = arr[N - 1];
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    let cnt = 1;
+    let pre = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] - pre < mid) continue;
+      cnt += 1;
+      pre = arr[i];
+    }
+
+    if (cnt < C) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return end;
+}
+
+console.log(solve(arr, C));
